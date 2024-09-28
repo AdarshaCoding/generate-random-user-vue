3b6d70fb620fe53bec641edf82c096ef9d360643
AdarshaCoding <adarshapc4467@gmail.com>
2024-09-28 08:41:08 +0530

    Hard coded sample user
    v-bind directory used when adding the user profile v-bind:src=image_url
    alternative way :src=image_url
    used template literals to display dynamic state values from state variables

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+//create Vue app using Vue.createApp function, which takes an object
 const app = Vue.createApp({
-  template: "<h1> {{message}} </h1>",
   data() {
     return {
-      message: "Hello World!",
+      firstName: "Adarsha",
+      lastName: "PC",
+      emailId: "adarsha@gmail.com",
+      gender: "male",
+      picture:
+        "https://images.vexels.com/content/145908/preview/male-avatar-maker-2a7919.png",
     };
   },
 });
 
+//mounting the application into root element id="app"
 app.mount("#app");
